4f8f2dafb2fb1fcfbf5df4f61a0516b1e8924e41
Júlio César Nardelli <jcnborges@jcn.borges-laptop-xxx>
2011-11-08 09:44:13 -0200

    Adicao do comando sync no protocolo

--- C8051F340/protocolo.h
+++ C8051F340/protocolo.h
@@ -4,32 +4,38 @@
 * enviados/recebidos pelo robo
 **************************/
 
-#define ON                0x0001
-#define OFF               0x0000
+#define ON					0x01
+#define OFF					0x00
 
-#define ENCODER					0x0020	 	
+// Encoder (encoder indice 0 -> 0x20, encoder 1 -> 0x21)
+#define ENCODER				0x20	 	
 
 // Sensores de distancia
-#define SENSOR_OPTICO_0       0x0022
-#define SENSOR_OPTICO_1       0x0023
-#define SENSOR_OPTICO_2       0x0024
-#define SENSOR_OPTICO_3       0x0025
-#define SENSOR_OPTICO_4       0x0026
-#define SENSOR_OPTICO_5       0x0027
+#define SENSOR_OPTICO_0		0x22
+#define SENSOR_OPTICO_1		0x23
+#define SENSOR_OPTICO_2		0x24
+#define SENSOR_OPTICO_3		0x25
+#define SENSOR_OPTICO_4		0x26
+#define SENSOR_OPTICO_5		0x27
 
 // Comandos para controlar PWM
-#define RODA_LEFT        0xA0 // Nivel das PWMS de Movimentacao ou passos
-#define RODA_RIGHT       0xA1 // Nivel das PWMS de Movimentacao ou passos
+#define RODA_LEFT			0xA0 // Nivel das PWMS de Movimentacao ou passos
+#define RODA_RIGHT			0xA1 // Nivel das PWMS de Movimentacao ou passos
+
+// Comando para sincronizacao
+#define SYNC				0xB0
 
 // Comando para fazer o robo parar imediatamente
-#define STOP             0xFF // Parada De Emergencia !!!!
+#define STOP				0xFF // Parada De Emergencia !!!!
 
-#define RESET           0x00F0 // Ordem para RESET GLOBAL
+#define RESET				0xF0 // Ordem para RESET GLOBAL
+
 // Byte que sinaliza fim do comando
-#define FIM_COMANDO	    0x00FE
+#define FIM_COMANDO			0xFE
 
 // Mascara para obter o valor do pwm e o sentido
 // o bit mais significativo representa o sentido,
 // enquanto os restantes representam o valor de PWM
-#define MASCARA_SENTIDO  0x0080  // Mascara que define o bit de sentido da PWM
-#define MASCARA_PWM      0x007F  // Mascara que define o bit de valor   da PWM
+#define MASCARA_SENTIDO		0x80  // Mascara que define o bit de sentido da PWM
+#define MASCARA_PWM			0x7F  // Mascara que define o bit de valor   da PWM
+
--- C8051F340/tslogic.cpp
+++ C8051F340/tslogic.cpp
@@ -12,26 +12,28 @@ int vLeft = 0, vRight = 0;
 int MIN(int a, int b){ return a < b ? a : b; }
 int MAX(int a, int b){ return a > b ? a : b; }
 
-void send_message(char cmd, char value){
+// Funcao para enviar uma mensagem para a C8051F340
+void send_message(char cmd, char value = 0){
 	putc(cmd, stdout);
 	putc(value, stdout);
 	putc(FIM_COMANDO, stdout);
 	putc(10, stdout);
 }
+
 // {{{ Funcoes de leitura
 void read_distances(){
 
-}
-
+} 
 void read_encoders(){
 
 }
 
+// }}} Fim funcoes leitura
+
+// {{{ Funcoes para controlar o robo
 void execute_action(){
 
 }
-// }}} Fim funcoes leitura
-// Funcao para alterar velocidade de uma roda
 void accelerate(int wheel, int dv){
 	if(wheel == LEFT){
 		vLeft = MAX(MIN(vLeft + dv, MAX_VELOCITY), 0);
@@ -42,17 +44,13 @@ void accelerate(int wheel, int dv){
 		send_message(RODA_RIGHT, vRight);
 	}
 }
-
+// }}}
 int main(void){
+// {{{ Loop de controle
 	for(int cnt = 0; ; ++cnt){
 		clock_t start = clock();
-		if(!((cnt / 15) & 1)){
-			accelerate(LEFT, 1);
-			accelerate(RIGHT, 1);
-		} else{
-			accelerate(LEFT, -1);
-			accelerate(RIGHT, -1);
-		}
+		// Envia o pedido de sincronizacao
+		send_message(SYNC);
 		// Ler as informacoes dos sensores de distancia read_distances();
 		read_distances();
 		// Ler as informacoes dos encoders
@@ -60,9 +58,17 @@ int main(void){
 		// Chamar algoritmo navegacao para tratar dados
 		// TODO
 		// Executa as acoes definidas pelo algoritmo
+		if(!((cnt / 15) & 1)){
+			accelerate(LEFT, 1);
+			accelerate(RIGHT, 1);
+		} else{
+			accelerate(LEFT, -1);
+			accelerate(RIGHT, -1);
+		}
 		execute_action();
 		// Espera completar 100ms para executar a rotina novamente
 		while((clock()- start)/(double)CLOCKS_PER_SEC < 0.5);
 	}
+// }}}
 	return 0;
 }
