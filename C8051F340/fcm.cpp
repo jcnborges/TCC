417858bd36a4f987c55b0ad592e7e9de428324a9
Julio C. N. Borges <jcn.borges@gmail.com>
2012-02-10 16:31:29 -0200

    Ajusta os pesos de acordo com um limiar da distancia normalizada 'sd'.

--- C8051F340/fcm.cpp
+++ C8051F340/fcm.cpp
@@ -10,6 +10,7 @@
 #define FRACA 0.125f
 #define MIN_DIST 14
 #define MAX_DIST 150
+#define LIMIAR 0.1
 #define SIG(x) (1 / (1 + exp(-10 * x + 4.5)))
 enum 
 {
@@ -33,22 +34,75 @@ float W[9][9];
 void init_W()
 {
 	W[SD][GDF] = FORTE; // w1 (+) FORTE
-	W[SD][GEF] = FRACA; // w2 (-) FRACA
-	W[SD][GET] = MEDIA; // w3 (+) MEDIA
+	W[SD][GEF] = FORTE; // w2 (+) FRACA
+	W[SD][GET] = FORTE; // w3 (+) MEDIA
 	
 	W[SE][GEF] = FORTE; // w4 (+) FORTE
-	W[SE][GDF] = FRACA; // w5 (-) FRACA
-	W[SE][GDT] = MEDIA; // w6 (+) MEDIA
+	W[SE][GDF] = FORTE; // w5 (+) FRACA
+	W[SE][GDT] = FORTE; // w6 (+) MEDIA
 	
-	W[SF][GDF] = MEDIA; // w7 (-) FORTE
+	W[SF][GDF] = MEDIA; // w7 (+) FORTE
 	W[SF][GDT] = MEDIA; // w8 (+) FRACA 
-	W[SF][GEF] = FORTE; // w9 (-) FORTE
-	W[SF][GET] = FORTE; // w10 (+) FRACA
+	W[SF][GEF] = MEDIA; // w9 (+) FORTE
+	W[SF][GET] = MEDIA; // w10 (+) FRACA
 
 	W[GDF][RD_Out] = FORTE; // w11 (+) FORTE
-	W[GDT][RD_Out] = MEDIA; // w12 (-) MEDIA
+	W[GDT][RD_Out] = FORTE; // w12 (-) MEDIA
 	W[GEF][RE_Out] = FORTE; // w13 (+) FORTE
-	W[GET][RE_Out] = MEDIA; // w14 (-) MEDIA
+	W[GET][RE_Out] = FORTE; // w14 (-) MEDIA
+
+	W[GDT][SD] = FRACA; // w15 (-) FRACA
+	W[GET][SE] = FRACA; // w16 (-) FRACA
+}
+
+void init_W(float sd)
+{
+	if (sd > LIMIAR)
+	{
+		W[SD][GDF] = FORTE; // w1 (+) FORTE
+		W[SD][GEF] = FORTE; // w2 (+) FRACA
+		W[SD][GET] = FORTE; // w3 (+) MEDIA
+		
+		W[SE][GEF] = FORTE; // w4 (+) FORTE
+		W[SE][GDF] = FORTE; // w5 (+) FRACA
+		W[SE][GDT] = FORTE; // w6 (+) MEDIA
+		
+		W[SF][GDF] = MEDIA; // w7 (-) MEDIA
+		W[SF][GDT] = MEDIA; // w8 (+) FRACA 
+		W[SF][GEF] = MEDIA; // w9 (-) MEDIA
+		W[SF][GET] = MEDIA; // w10 (+) FRACA
+
+		W[GDF][RD_Out] = FORTE; // w11 (+) FORTE
+		W[GDT][RD_Out] = FORTE; // w12 (-) MEDIA
+		W[GEF][RE_Out] = FORTE; // w13 (+) FORTE
+		W[GET][RE_Out] = FORTE; // w14 (-) MEDIA
+
+		W[GDT][SD] = FRACA; // w15 (-) FRACA
+		W[GET][SE] = FRACA; // w16 (-) FRACA
+	}
+	else
+	{		
+		W[SD][GDF] = FRACA; // w1 (+) FRACO
+		W[SD][GEF] = FORTE; // w2 (+) FORTE
+		W[SD][GET] = FRACA; // w3 (+) FRACA
+		
+		W[SE][GEF] = FORTE; // w4 (+) FORTE
+		W[SE][GDF] = FRACA; // w5 (+) FRACA
+		W[SE][GDT] = FORTE; // w6 (+) MEDIA
+		
+		W[SF][GDF] = FRACA; // w7 (+) FRACA
+		W[SF][GDT] = FORTE; // w8 (+) FORTE 
+		W[SF][GEF] = MEDIA; // w9 (+) FORTE
+		W[SF][GET] = FRACA; // w10 (+) FRACA
+
+		W[GDF][RD_Out] = FORTE; // w11 (+) FORTE
+		W[GDT][RD_Out] = FORTE; // w12 (-) MEDIA
+		W[GEF][RE_Out] = FORTE; // w13 (+) FORTE
+		W[GET][RE_Out] = FORTE; // w14 (-) MEDIA
+
+		W[GDT][SD] = FRACA; // w15 (-) FRACA
+		W[GET][SE] = FORTE; // w16 (-) FORTE
+	}
 }
 
 void inference(float se, float sf, float sd, float &rd_out, float &re_out)
@@ -60,7 +114,10 @@ void inference(float se, float sf, float sd, float &rd_out, float &re_out)
 	sd = (sd - MIN_DIST) / float(MAX_DIST - MIN_DIST);
 	sf = (sf - MIN_DIST) / float(MAX_DIST - MIN_DIST);
 	se = (se - MIN_DIST) / float(MAX_DIST - MIN_DIST);
-	
+
+	// inicializa os pesos de acordo com a distancia sd
+	init_W(sd);
+
 	// calcula o valor do conceito gdf
 	gdf = W[SD][GDF] * (1 - SIG(sd));
 	gdf += W[SF][GDF] * SIG(sf);
@@ -71,6 +128,7 @@ void inference(float se, float sf, float sd, float &rd_out, float &re_out)
 	// calcula o valor do conceito gdt
 	gdt = W[SE][GDT] * (1 - SIG(se));
 	gdt += W[SF][GDT] * (1 - SIG(sf));
+	gdt -= W[SD][GDT] * (1 - SIG(sd));
 	gdt /= W[SE][GDT] + W[SF][GDT];
 
 	// calcula o valor do conceito gef
@@ -83,6 +141,7 @@ void inference(float se, float sf, float sd, float &rd_out, float &re_out)
 	// calcula o valor do conceito get
 	get = W[SD][GET] * (1 - SIG(sd));
 	get += W[SF][GET] * (1 - SIG(sf));
+	get -= W[SE][GET] * (1 - SIG(se));
 	get /= W[SD][GET] + W[SF][GET];
 
 	printf("gdf = %.5lf\ngef = %.5lf\n", gdf, gef);
